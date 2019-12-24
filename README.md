12716e7e9f697991cd4ebda4f58a905d731bf0f4
zhangyuang <leonin0829@gmail.com>
2019-12-24 10:52:30 +0800

    test: add unit test coverage 100%

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ English | [简体中文](./README_zh-CN.md)
 
 # Download specified directory in github
 
+<a href="https://circleci.com/gh/zhangyuang"><img src="https://img.shields.io/circleci/build/github/zhangyuang/dclone/master.svg" alt="Build Status"></a>
+<a href="https://codecov.io/gh/zhangyuang/dclone"><img src="https://codecov.io/gh/zhangyuang/dclone/branch/master/graph/badge.svg" alt="Coverage Status"></a>
+
 dclone is the simplest way to download specified directory in github depend on git, use dclone command can cut down download time
 
 ![](./image/time.png)
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,47 @@
-import { execSync }from 'child_process'
+import fs from 'fs'
+import Shell from 'shelljs'
+import { dclone } from '../src/dclone'
 
 jest.setTimeout(10000)
-
+// @ts-ignore
+const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {
+    //
+})
+// @ts-ignore
+const mockError = jest.spyOn(console, 'error').mockImplementation(() => {
+    //
+})
+jest.mock('inquirer', () => ({
+  prompt: jest.fn().mockReturnValueOnce({
+    delete: true
+  }).mockReturnValueOnce({
+    delete: false
+  })
+}))
 describe('test dclone', () => {
-    test('expect ssr-with-loadable can be download', async () => {
-        execSync('ts-node ../src/index https://github.com/ykfe/egg-react-ssr/tree/dev/example/ssr-with-loadable')
-    })
+  beforeEach(() => {
+    Shell.mkdir('-p','temp/example')
+    Shell.cd('temp')
+  })
+  test('not input dir should exit', async () => {
+    await dclone()
+    expect(mockError).toBeCalled()
+  })
+  test('expect ssr-with-loadable can be download', async () => {
+    await dclone('https://github.com/ykfe/egg-react-ssr/tree/dev/example/ssr-with-loadable')
+    expect(fs.existsSync('./example')).toBe(true)
+    expect(fs.existsSync('./example/ssr-with-loadable')).toBe(true)
+  })
+  test('process.exit should be called', async () => {
+    await dclone('https://github.com/ykfe/egg-react-ssr/tree/dev/example/ssr-with-loadable')
+    expect(mockExit).toBeCalled()
+  })
+  test('rootDir can be git clone', async () => {
+    await dclone('https://github.com/ykfe/egg-react-ssr')
+    expect(fs.existsSync('./egg-react-ssr')).toBe(true)
+  })
+  afterEach(() => {
+    Shell.cd('../')
+    Shell.rm('-rf','temp')
+  })
 })
