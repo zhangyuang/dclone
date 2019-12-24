3b14da0f26bb529965a0ba5e77b373ed4bce6679
zhangyuang <leonin0829@gmail.com>
2019-12-24 15:25:58 +0800

    release: 1.0.7

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
+import Shell from 'shelljs'
 import { argv } from 'optimist'
 import { dclone } from './dclone'
 
 const dir = argv._[0]
 dclone(dir).catch(err => {
+  Shell.rm('-rf', '.git')
   console.log(err)
+  process.exit()
+})
+
+process.on('SIGINT', () => {
+  Shell.rm('-rf', '.git')
+  console.log('进程中途终止')
   process.exit()
 })
