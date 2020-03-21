741a97941c0a302e07618f3aa0c62a536201138b
zhangyuang <leonin0829@gmail.com>
2020-03-22 00:08:26 +0800

    chore: add init.ts

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,4 @@
-#!/usr/bin/env node
-
-import Shell from 'shelljs'
-import { argv } from 'optimist'
 import { dclone } from './dclone'
-const { version } = require('../package.json')
-const dir = argv._[0]
-
-if (argv.v) {
-  Shell.echo(version)
-  process.exit()
-}
-
-dclone(dir).catch(err => {
-  Shell.rm('-rf', '.git')
-  console.log(err)
-  process.exit()
-})
-
-process.on('SIGINT', () => {
-  Shell.rm('-rf', '.git')
-  console.log('进程中途终止')
-  process.exit()
-})
 
 export {
   dclone
