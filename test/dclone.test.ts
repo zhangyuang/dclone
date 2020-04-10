fed26529023c54a4009bf3a3f6b143ea9716d539
yuang.zya <yuang.zya@alibaba-inc.com>
2020-04-10 16:19:17 +0800

    fix: update test

--- test/dclone.test.ts
+++ test/dclone.test.ts
@@ -24,26 +24,36 @@ describe('test dclone', () => {
     Shell.cd('temp')
   })
   test('not input dir should exit', async () => {
-    await dclone()
+    await dclone({
+      dir: ''
+    })
     expect(mockError).toBeCalled()
   })
   test('expect ssr-with-loadable can be download', async () => {
     Shell.mkdir('-p','example')
-    await dclone('https://github.com/ykfe/egg-react-ssr/tree/dev/example/ssr-with-loadable')
+    await dclone({
+      dir: 'https://github.com/ykfe/egg-react-ssr/tree/dev/example/ssr-with-loadable'
+    })
     expect(fs.existsSync('./example')).toBe(true)
     expect(fs.existsSync('./example/ssr-with-loadable')).toBe(true)
   })
   test('process.exit should be called', async () => {
     Shell.mkdir('-p','example')
-    await dclone('https://github.com/ykfe/egg-react-ssr/tree/dev/example/ssr-with-loadable')
+    await dclone({
+      dir: 'https://github.com/ykfe/egg-react-ssr/tree/dev/example/ssr-with-loadable'
+    })
     expect(mockExit).toBeCalled()
   })
   test('rootDir can be git clone ', async () => {
-    await dclone('https://github.com/ykfe/egg-react-ssr')
+    await dclone({
+      dir: 'https://github.com/ykfe/egg-react-ssr'
+    })
     expect(fs.existsSync('./egg-react-ssr')).toBe(true)
   })
   test('url has multiply tree can clone succeed', async (done) => {
-    await dclone('https://github.com/ReactiveX/rxjs/tree/experiment-trex/doc/decision-tree-widget')
+    await dclone({
+      dir: 'https://github.com/ReactiveX/rxjs/tree/experiment-trex/doc/decision-tree-widget'
+    })
     expect(fs.existsSync('./doc')).toBe(true)
     expect(fs.existsSync('./doc/decision-tree-widget')).toBe(true)
     done()
